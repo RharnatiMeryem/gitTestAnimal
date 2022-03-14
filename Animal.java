15174aedfe342e79b7f24d7e63931df8cec231e8
Meryem Rharnati <mrharnati@smb.sqli.com>
2022-03-14 14:39:53 +0100

    initial commit

---
+++ Animal.java
@@ -0,0 +1,10 @@
+public class Animal {
+
+    public static String getDefaultGreeting() {
+        return "Hello!";
+    }
+
+    public static void main(String[] args) {
+        getDefaultGreeting();
+    }
+}
